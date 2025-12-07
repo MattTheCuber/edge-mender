7316700a133d74545145e376297aedb73e6f23b8
Matthew Vine <mattvine03@gmail.com>
2025-12-07 14:08:07 -0500

    docs(config): add reference for pytest config

--- conftest.py
+++ conftest.py
@@ -4,7 +4,12 @@ import pytest
 
 
 def pytest_addoption(parser: pytest.Parser) -> None:
-    """Add command line options for pytest."""
+    """Add command line options for pytest.
+
+    References
+    ----------
+    .. [1] https://docs.pytest.org/en/stable/example/simple.html#control-skipping-of-tests-according-to-command-line-option
+    """
     parser.addoption(
         "--slow",
         action="store_true",
@@ -14,7 +19,12 @@ def pytest_addoption(parser: pytest.Parser) -> None:
 
 
 def pytest_configure(config: pytest.Config) -> None:
-    """Configure pytest with custom markers."""
+    """Configure pytest with custom markers.
+
+    References
+    ----------
+    .. [1] https://docs.pytest.org/en/stable/example/simple.html#control-skipping-of-tests-according-to-command-line-option
+    """
     config.addinivalue_line("markers", "slow: mark test as slow to run")
 
 
@@ -22,7 +32,12 @@ def pytest_collection_modifyitems(
     config: pytest.Config,
     items: list[pytest.Item],
 ) -> None:
-    """Modify collected test items to skip slow tests unless --slow is given."""
+    """Modify collected test items to skip slow tests unless --slow is given.
+
+    References
+    ----------
+    .. [1] https://docs.pytest.org/en/stable/example/simple.html#control-skipping-of-tests-according-to-command-line-option
+    """
     if config.getoption("--slow"):
         # --slow given in cli: do not skip slow tests
         return
